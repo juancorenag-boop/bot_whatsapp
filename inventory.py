72f25d006e4cb19b5864943ef1f17e44854fd937
juancorenag-boop <juancorenag@gmail.com>
2026-01-29 10:43:34 -0500

    agregar inventory.py

---
+++ inventory.py
@@ -0,0 +1,7 @@
+INVENTARIO = [
+    {"tipo": "arroz", "marca": "roa", "precio": 4500, "stock": 10},
+    {"tipo": "arroz", "marca": "diana", "precio": 5000, "stock": 10},
+    {"tipo": "leche", "marca": "ciledco", "precio": 2000, "stock": 5},
+    {"tipo": "leche", "marca": "karen", "precio": 3000, "stock": 2},
+    {"tipo": "leche", "marca": "alqueria", "precio": 4000, "stock": 3},
+]
