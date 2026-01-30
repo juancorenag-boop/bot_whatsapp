201231e7396bc2c816d841f353ce76ecdbf6b5c8
juancorenag-boop <juancorenag@gmail.com>
2026-01-30 18:25:00 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,7 +1,17 @@
+
 INVENTARIO = [
+    # ===== ABARROTES =====
     {"tipo": "arroz", "marca": "roa", "precio": 4500, "stock": 10},
     {"tipo": "arroz", "marca": "diana", "precio": 5000, "stock": 10},
     {"tipo": "leche", "marca": "ciledco", "precio": 2000, "stock": 5},
     {"tipo": "leche", "marca": "karen", "precio": 3000, "stock": 2},
     {"tipo": "leche", "marca": "alqueria", "precio": 4000, "stock": 3},
+
+    # ===== VERDURAS (POR LIBRA) =====
+    {"tipo": "tomate", "marca": "", "precio": 1800, "unidad": "libra"},
+    {"tipo": "papa", "marca": "", "precio": 1200, "unidad": "libra"},
+    {"tipo": "cebolla", "marca": "", "precio": 1500, "unidad": "libra"},
+    {"tipo": "zanahoria", "marca": "", "precio": 1300, "unidad": "libra"},
+    {"tipo": "banano", "marca": "", "precio": 1200, "unidad": "libra"},
 ]
+
