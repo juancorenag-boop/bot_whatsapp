4c4b6bdb65dd3669f7235812c83e4a1bc5c5237d
juancorenag-boop <juancorenag@gmail.com>
2026-01-30 09:52:27 -0500

    Update search.py

--- search.py
+++ search.py
@@ -1,21 +1,14 @@
-def buscar(texto, inventario):
-    texto = texto.lower()
-    palabras = texto.split()
+from inventory import INVENTARIO
 
+def buscar(texto):
+    texto = texto.lower()
     resultados = []
 
-    for p in inventario:
-        score = 0
-        for palabra in palabras:
-            if palabra in p["tipo"]:
-                score += 2
-            if palabra in p["marca"]:
-                score += 3
-            if palabra == "barato":
-                score += max(0, 5000 - p["precio"]) / 1000
+    for p in INVENTARIO:
+        if (
+            texto in p["tipo"].lower()
+            or texto in p["marca"].lower()
+        ) and p["stock"] > 0:
+            resultados.append(p)
 
-        if score > 0:
-            resultados.append((score, p))
-
-    resultados.sort(reverse=True, key=lambda x: x[0])
-    return [p for _, p in resultados]
+    return resultados
