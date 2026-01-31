673b4fbe6cff94d065367d731386fba289b19f4c
juancorenag-boop <juancorenag@gmail.com>
2026-01-31 11:53:27 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,10 +1,15 @@
+# responses.py
+
+def saludo():
+    return "👋 Bienvenido a la tienda Escribe el producto que buscas: Ej: arroz, leche, tomate"
+
 def lista_productos(resultados):
     texto = "🛒 Productos disponibles:\n"
     for i, p in enumerate(resultados, start=1):
         marca = p.get("marca","")
-        if marca:  # si hay marca
+        if marca:
             texto += f"{i}. {p['tipo'].title()} {marca.title()} - ${p['precio']} (Stock: {p.get('stock', 0)})\n"
-        else:  # si no hay marca
+        else:
             texto += f"{i}. {p['tipo'].title()} - ${p['precio']} (Stock: {p.get('stock', 0)})\n"
     texto += "Responde con el número para agregarlo."
     return texto
