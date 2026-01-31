a1d715e64e08c4096ee32aa09fa12d7d40e8bf88
juancorenag-boop <juancorenag@gmail.com>
2026-01-31 14:00:34 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -8,10 +8,10 @@ INVENTARIO = [
     {"tipo": "leche", "marca": "alqueria", "precio": 4000, "stock": 3},
 
     # ===== VERDURAS (POR LIBRA) =====
-    {"tipo": "tomate", "marca": "", "precio": 1800, "unidad": "libra"},
-    {"tipo": "papa", "marca": "", "precio": 1200, "unidad": "libra"},
-    {"tipo": "cebolla", "marca": "", "precio": 1500, "unidad": "libra"},
-    {"tipo": "zanahoria", "marca": "", "precio": 1300, "unidad": "libra"},
-    {"tipo": "banano", "marca": "", "precio": 1200, "unidad": "libra"},
+    {"tipo": "tomate", "marca": "", "precio": 1800, "unidad": "libra","stock": 10},
+    {"tipo": "papa", "marca": "", "precio": 1200, "unidad": "libra","stock": 20},
+    {"tipo": "cebolla", "marca": "", "precio": 1500, "unidad": "libra", "stock": 20},
+    {"tipo": "zanahoria", "marca": "", "precio": 1300, "unidad": "libra", "stock": 15},
+    {"tipo": "banano", "marca": "", "precio": 1200, "unidad": "libra", "stock": 13},
 ]
 
