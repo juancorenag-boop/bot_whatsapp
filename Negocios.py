5282b905c6fda2c33d46681bfd23fd0c167106e1
juancorenag-boop <juancorenag@gmail.com>
2026-02-07 11:13:56 -0500

    Update and rename inventory.py to Negocios.py

---
+++ Negocios.py
@@ -0,0 +1,39 @@
+# Negocios.py
+BUSINESSES = {
+    "1": {
+        "name": "Tienda La Esquina",
+        "type": "store",
+        "phone": "573216642926",
+
+        "inventory": [
+            # ===== ABARROTES =====
+            {"tipo": "arroz", "marca": "roa", "precio": 4500, "stock": 10},
+            {"tipo": "arroz", "marca": "diana", "precio": 5000, "stock": 10},
+
+            {"tipo": "leche", "marca": "ciledco", "precio": 2000, "stock": 5},
+            {"tipo": "leche", "marca": "karen", "precio": 3000, "stock": 2},
+            {"tipo": "leche", "marca": "alqueria", "precio": 4000, "stock": 3},
+
+            # ===== VERDURAS (POR LIBRA) =====
+            {"tipo": "tomate", "marca": "", "precio": 1800, "unidad": "libra", "stock": 10},
+            {"tipo": "papa", "marca": "", "precio": 1200, "unidad": "libra", "stock": 20},
+            {"tipo": "cebolla", "marca": "", "precio": 1500, "unidad": "libra", "stock": 20},
+            {"tipo": "zanahoria", "marca": "", "precio": 1300, "unidad": "libra", "stock": 15},
+            {"tipo": "banano", "marca": "", "precio": 1200, "unidad": "libra", "stock": 13},
+        ]
+    },
+# ======================= RESTAURANTES ===================================================================
+
+    "2": {
+        "name": "Restaurante Donde Kati",
+        "type": "restaurant",
+        "phone": "573104567890",
+
+        "menu": [
+            {"tipo": "carne asada", "precio": 20000},
+            {"tipo":"cerdo asado", "precio":18000}
+            {"tipo": "gaseosa", "precio": 5000},
+        ]
+    }
+}
+
